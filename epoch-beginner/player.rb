1bd0262d9ef933e24051bf5f0c74533ab744d331
Carter Bancroft <alt234@gmail.com>
2010-07-06 14:30:30 -0800

    Made it to 4.

--- epoch-beginner/player.rb
+++ epoch-beginner/player.rb
@@ -1,7 +1,15 @@
 class Player
   def play_turn(warrior)
     @warrior = warrior
-    feel.empty? ? walk! : attack!
+	if feel.empty? and health > 10
+		walk!
+	elsif feel.empty? and health < 10
+		rest!
+	elsif !feel.empty?
+		attack!
+	else
+		walk!
+	end
   end
 
   # Get rid of the warrior.everything
