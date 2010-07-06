8acbe9dee69d4342e291f8d3912ba5ba1f5e2a5e
Jason Jones <jasonj@resdat.com>
2010-07-06 12:59:37 -0800

    Level 1

--- epoch-beginner/player.rb
+++ epoch-beginner/player.rb
@@ -1,5 +1,11 @@
 class Player
   def play_turn(warrior)
-    # add your code here
+    @warrior = warrior
+    walk!
+  end
+
+  # Get rid of the warrior.everything
+  def method_missing(sym, *args, &block)
+    @warrior.send(sym, *args, &block)
   end
 end
