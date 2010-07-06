8a07e2cd3d7203e9d1ff0919e7123e606eef0a1d
Jason Jones <jasonj@resdat.com>
2010-07-06 14:43:14 -0800

    Update README and refactor player a bit.

--- epoch-beginner/player.rb
+++ epoch-beginner/player.rb
@@ -1,19 +1,23 @@
 class Player
   def play_turn(warrior)
     @warrior = warrior
-	if feel.empty? and health > 10
-		walk!
-	elsif feel.empty? and health < 10
-		rest!
-	elsif !feel.empty?
-		attack!
-	else
-		walk!
-	end
+    tick
   end
 
   # Get rid of the warrior.everything
   def method_missing(sym, *args, &block)
     @warrior.send(sym, *args, &block)
+  end
+
+  def tick
+    if feel.empty? and health > 10
+      walk!
+    elsif feel.empty? and health < 10
+      rest!
+    elsif !feel.empty?
+      attack!
+    else
+      walk!
+    end
   end
 end
