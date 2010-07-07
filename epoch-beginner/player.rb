249bd5f0b01a00aeda2ecbb1bb549712542a884c
unknown <Administrator@.(none)>
2010-07-07 14:44:54 -0800

    Lvl 6 complete

--- epoch-beginner/player.rb
+++ epoch-beginner/player.rb
@@ -19,14 +19,17 @@ class Player
       :minimum_health => 9,
       :maximum_health => 20
     }
+	@direction = :backward
   end
 
   def tick
-    walk! :backward and return if under_attack? and low_health? 
-    rest! and return if not fully_healed? and not under_attack? and feel.empty?
-	rescue! and return if feel.captive?
-    attack! and return if not feel.empty? and not feel.captive?
-    walk! and return
+	
+    walk! backwards and return if under_attack? and low_health? 
+    rest! and return if not fully_healed? and not under_attack? and feel(@direction).empty?
+	rescue! @direction and return if feel(@direction).captive?
+	walk! reverse_direction and return if feel(@direction).wall?
+    attack! @direction and return if not feel(@direction).empty? and not feel(@direction).captive?
+    walk! @direction and return
   end
 
   def under_attack?
@@ -39,6 +42,12 @@ class Player
 
   def fully_healed?
     health >= @config[:maximum_health]
+  end
+  def reverse_direction
+    @direction = (@direction == :forward) ? :backward : :forward
+  end
+  def backwards
+	(@direction == :forward) ? :backward : :forward
   end
 
 end
